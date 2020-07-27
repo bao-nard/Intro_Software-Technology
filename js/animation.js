831558997cddcad4a74e990eb7fc7cdd07540b91
Bao_Khanh <nh184496@gmail.com>
2020-07-28 06:04:32 +0700

    Signed-off-by: Bao_Khanh <nh184496@gmail.com>

---
+++ js/animation.js
@@ -0,0 +1,8 @@
+function myClick() {
+    var x = confirm("Xác nhận mua mặt hàng này");
+    if (x) {
+        alert("Cảm ơn bạn tin tưởng sản phẩm của chúng tôi");
+    } else {
+        alert("Mời bạn quay lại tham khảo sản phẩm khác");
+    }
+}
